d155216336692fcb6a58760bc48aefe648845f9b
devNinja254 <aga.imbali1@gmail.com>
2024-12-16 19:06:07 +0300

    rm migrations

--- flix/settings.py
+++ flix/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static'),
+     os.path.join(BASE_DIR, 'statics/'),
  ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
 INTERNAL_IPS = [
@@ -163,3 +163,4 @@ EMAIL_POST = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER ='aga.imbali1@gmail.com'
 EMAIL_HOST_PASSWORD ='oyqtbwumfqecasik'
+
